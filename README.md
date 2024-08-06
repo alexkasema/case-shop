7f205b67972783d1826b0c6fe4b4192a745bd035
AlexKasema <lexxmulwa@gmail.com>
2024-08-06 15:05:50 +0300

    Created the DesignConfigurator component for all the design logic. Implemented resizability of the image on the case. Implemeted Color customization of the case.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Case Shop
 
-This is a Shopping Application where buy people can buy phone cases which are customized with the image of their choice
+This is a Shopping Application whereby people can buy phone cases which are customized with the image of their choice
--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,3 +1,7 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignConfigurator from "./DesignConfigurator";
+
 interface PageProps {
   searchParams: {
     [key: string]: string | string[] | undefined;
@@ -7,7 +11,27 @@ interface PageProps {
 const DesignPage = async ({ searchParams }: PageProps) => {
   const { id } = searchParams;
 
-  return <div>{id}</div>;
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, width, height } = configuration;
+
+  return (
+    <DesignConfigurator
+      configId={configuration.id}
+      imageDimensions={{ width, height }}
+      imageUrl={imageUrl}
+    />
+  );
 };
 
 export default DesignPage;
