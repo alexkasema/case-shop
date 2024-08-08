64bfae6421c21354ce30cbb7d163bf491f87ad4b
AlexKasema <lexxmulwa@gmail.com>
2024-08-08 21:19:02 +0300

    Created an Order received confirmation for the user using resend

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,8 +1,12 @@
+import OrderReceivedEmail from "@/components/emails/OrderReceivedEmail";
 import { db } from "@/db";
 import { stripe } from "@/lib/stripe";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
+import { Resend } from "resend";
 import Stripe from "stripe";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
   try {
@@ -67,6 +71,25 @@ export async function POST(req: Request) {
             },
           },
         },
+      });
+
+      await resend.emails.send({
+        from: "CaseShop <lexmulwa@gmail.com>",
+        to: [event.data.object.customer_details.email],
+        subject: "Thanks for your order!",
+        react: OrderReceivedEmail({
+          orderId,
+          orderDate: updatedOrder.createdAt.toLocaleDateString(),
+          // @ts-ignore
+          shippingAddress: {
+            name: session.customer_details!.name!,
+            city: shippingAddress!.city!,
+            country: shippingAddress!.country!,
+            postalCode: shippingAddress!.postal_code!,
+            street: shippingAddress!.line1!,
+            state: shippingAddress!.state,
+          },
+        }),
       });
 
       return NextResponse.json({ result: event, ok: true });
