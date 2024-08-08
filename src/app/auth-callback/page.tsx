4ea26b48b48ef9f92c2d8c8f9abafd83ed517090
AlexKasema <lexxmulwa@gmail.com>
2024-08-08 11:56:34 +0300

    Implemented the stripe checkout process and created the user,order, shippingAddress, billingAddress database modals

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+const AuthCallbackPage = () => {};
+
+export default AuthCallbackPage;
---
+++ src/app/configure/preview/actions.ts
@@ -0,0 +1,88 @@
+"use server";
+
+import { BASE_PRICE, PRODUCT_PRICES } from "@/config/products";
+import { db } from "@/db";
+import { stripe } from "@/lib/stripe";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { Order } from "@prisma/client";
+
+export const createCheckoutSession = async ({
+  configId,
+}: {
+  configId: string;
+}) => {
+  const configuration = await db.configuration.findUnique({
+    where: { id: configId },
+  });
+
+  if (!configuration) {
+    throw new Error("No such configuration found");
+  }
+
+  //! checkout session is going to be user specific
+  //! we need to know which user purchased this item
+
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) {
+    throw new Error("You need to be logged in");
+  }
+
+  const { finish, material } = configuration;
+
+  let price = BASE_PRICE;
+  if (finish === "textured") price += PRODUCT_PRICES.finish.textured;
+  if (material === "polycarbonate")
+    price += PRODUCT_PRICES.material.polycarbonate;
+
+  //! creating the actual order
+  let order: Order | undefined = undefined;
+
+  const existingOrder = await db.order.findFirst({
+    where: {
+      userId: user.id,
+      configurationId: configuration.id,
+    },
+  });
+
+  console.log(user.id, configuration.id);
+
+  if (existingOrder) {
+    order = existingOrder;
+  } else {
+    order = await db.order.create({
+      data: {
+        amount: price / 100,
+        userId: user.id,
+        configurationId: configuration.id,
+      },
+    });
+  }
+
+  //! we need to know the product the user is buying
+  const product = await stripe.products.create({
+    name: "Custom iPhone Case",
+    images: [configuration.imageUrl],
+    default_price_data: {
+      currency: "USD",
+      unit_amount: price,
+    },
+  });
+
+  //! create stripe payment session
+  const stripeSession = await stripe.checkout.sessions.create({
+    success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
+    cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
+    payment_method_types: ["card"],
+    mode: "payment",
+    shipping_address_collection: { allowed_countries: ["KE", "US"] },
+    metadata: {
+      userId: user.id,
+      orderId: order.id,
+    },
+    line_items: [{ price: product.default_price as string, quantity: 1 }],
+  });
+
+  return { url: stripeSession.url };
+};
